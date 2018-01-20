03d06f266094a9152f46ff67cd63e2fd1248954e
Egor Tamarin <edtamarin@gmail.com>
2018-01-20 16:46:18 +0100

    Added new UI elements, fixed time display
    Added satellites in LOS, heading, position. Time in minutes now preceded
    with a zero.

--- BikeGPS/BikeGPS.ino
+++ BikeGPS/BikeGPS.ino
@@ -7,50 +7,95 @@
 #include <SoftwareSerial.h>
 
 /**
- * The mobile part that tracks the movement
- * Board: WeMos D1 mini
- * 
- * Egor Tamarin, 2017
- */
+   The mobile part that tracks the movement
+   Board: WeMos D1 mini
+
+   Egor Tamarin, 2017
+*/
 
 SSD1306 display(0x3C, D3, D5);
 TinyGPSPlus gps;
-SoftwareSerial ss(D1,D2);
+SoftwareSerial ss(D1, D2);
 
-String timeDisplay[2];
+// compass rose
+String compass[17] = {"N","NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW","N"};
+// display strings
+String upperBar[4] = {"SAT in range:", "X", "XX", "XX"};
+String heading = "FIX";
+String pos[2] = {"NO","FIX"};
 
-int timeCoords[2] = {85,15};
+// coordinates for the UI
+int timeCoords[2] = {100, 0};
+int hdngCoords[2] = {0, 15};
+int posCoords[2] = {0,50};
 
 void setup() {
-  // put your setup code here, to run once:
+  // start the serial communication
   ss.begin(9600);
   Serial.begin(115200); // serial is used for debugging
-  display.init();
-  display.setFont(ArialMT_Plain_10);
+  display.init(); // start the display
+  display.setFont(ArialMT_Plain_10); // display settings
   display.flipScreenVertically();
   display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(0,0,"Initializing...");
-  display.display();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // clear the display buffer so we don't write over anything
   display.clear();
+  // GPS is polled at 1 HZ, so double the poll rate is enough fo a smooth data update rate
   smartDelay(500);
-  if (gps.location.isUpdated()){
-    Serial.print("LAT="); Serial.print(gps.location.lat(), 6);
-    Serial.print("LNG="); Serial.println(gps.location.lng(), 6);
-  }
+  // update all the data on screen
+  // it will not change if nothing new was received
   updateTime();
+  updateSatNum();
+  updateHdng();
+  updatePos();
+  // update the display
   display.display();
 }
 
-void updateTime(){
-  if (gps.time.isUpdated()){
-  timeDisplay[0] = String(gps.time.hour());
-  timeDisplay[1] = String(gps.time.minute());
+// update the time
+void updateTime() {
+  if (gps.time.isUpdated()) {
+    upperBar[2] = String(gps.time.hour());
+    upperBar[3] = String(gps.time.minute());
+    if (gps.time.minute() < 10) upperBar[3] = "0" + upperBar[3];
   }
-  display.drawString(timeCoords[0],timeCoords[1],timeDisplay[0]+":"+timeDisplay[1]);
+  display.drawString(timeCoords[0], timeCoords[1], upperBar[2] + ":" + upperBar[3]);
+}
+
+// update the number of satellites in LOS
+void updateSatNum() {
+  if (gps.satellites.isUpdated()) {
+    upperBar[1] = String(gps.satellites.value());
+  }
+  display.drawString(0, 0, upperBar[0] + " " + upperBar[1]);
+}
+
+// update the heading
+void updateHdng() {
+  if (gps.course.isUpdated()) {
+    heading = String(compassRose(gps.course.deg()));
+  }
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(hdngCoords[0], hdngCoords[1], heading);
+  display.setFont(ArialMT_Plain_10);
+}
+
+// update the current position
+void updatePos() {
+  if (gps.location.isUpdated()) {
+    pos[0] = String(gps.location.lat(), 3);
+    pos[1] = String(gps.location.lng(), 3);
+  }
+  display.drawString(posCoords[0],posCoords[1],pos[0]+";"+pos[1]);
+}
+
+// translate degree heading into a compass rose
+String compassRose(double degree){
+  int index = (int)degree%350;
+  index = round(index/22.5);
+  return compass[index];
 }
 
 // This custom version of delay() ensures that the gps object
@@ -58,7 +103,7 @@ void updateTime(){
 static void smartDelay(unsigned long ms)
 {
   unsigned long start = millis();
-  do 
+  do
   {
     while (ss.available())
       gps.encode(ss.read());
